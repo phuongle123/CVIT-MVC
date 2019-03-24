6cd620dc9894a53087e1a916acdc8f8391f8b7ca
phuongle123 <32607417+phuongle123@users.noreply.github.com>
2019-03-24 11:57:49 +0700

    11:57AM 24/03/2019

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php 
+DEFINE('INDEX_URL','http://localhost/CVIT_MVC/');
+
+DEFINE('DB_HOST','localhost');
+DEFINE('DB_USERNAME','root');
+DEFINE('DB_PASSWORD','');
+DEFINE('DB_NAME','do-an-3');
+
+DEFINE('CONTROLLER_DEFAULT','index');
+DEFINE('ACTION_DEFAULT','index');
+?>
